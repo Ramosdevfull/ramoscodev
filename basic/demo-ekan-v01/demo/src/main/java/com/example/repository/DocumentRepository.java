9fc4287139762ad5f0f60de0c6991e5832fcb9cc
Ademario Ramos <ramoscodev@gmail.com>
2024-04-01 10:48:11 -0300

    Resolved merge conflicts

--- basic/demo-ekan-v01/demo/src/main/java/com/example/repository/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.model.Document;
-
-public interface DocumentRepository extends JpaRepository<Document, Long> {
-    
-}
--- basic/demo-ekan/demo/src/main/java/com/example/repository/CustomUpdateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Beneficiary;
-
-public interface CustomUpdateRepository {
-    Beneficiary updateCustom(Beneficiary b);
-}
